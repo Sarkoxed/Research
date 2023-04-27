474ace3219ef0fd0e6e15642da25b9f2d8a9b837
Sarkoxed <sarkoxed2013@yandex.ru>
2023-04-27 19:36:50 +0300

    generalisations

--- shaparts/sigma/tools/verify.py
+++ shaparts/sigma/tools/verify.py
@@ -2,73 +2,85 @@ from z3 import BitVec, Solver, sat, Or
 import json
 from sys import argv
 
+def find_all_solutions(s, map, vars, bound):
+    w = []
+    i = 0
+    print("___________________________________________")
+    print("Найденные input:")
+    if argv[1] == "-1":
+        if s.check() == sat:
+            m = s.model()
+            for x in vars:
+                w.append(str(m[x]))
+            with open("twitness/witness.json", "wt") as f:
+                json.dump(w, f)
+            with open("data/calculated_input.json", "wt") as f:
+                json.dump([w[x] for x in range(len(w)) if "main.in" in map[x]], f)
+        else:
+            print("unsat")
 
-constrs = json.load(open("constraints/constr.json"))
-witness = json.load(open("witness/witness.json"))
-map = json.load(open("data/map.json"))["map"]
+    elif argv[1] == "--all":
+        while s.check() == sat:
+            m = s.model()
+            for x in vars:
+                w.append(str(m[x]))
 
-nvars = constrs["nVars"]
-nout = constrs["nOutputs"]
-ninp = constrs["nPrvInputs"]
+            print("".join([w[i] for i in range(len(w)) if "main.in" in map[i]]))  # len(w))
+            with open(f"calculations/input{str(i).zfill(2)}.json", "wt") as f:
+                json.dump(
+                    {"in": [w[x] for x in range(len(w)) if "main.in" in map[x]]}, f
+                )
 
-p = int(constrs["prime"])
-
-out = [int(x) for x in witness[1 : 1 + nout]]
-# print(out)
-
-vars = [BitVec(f"var_{i}", 1) for i in range(nvars)]
-
-s = Solver()
-s.add(vars[0] == 1)
-
-for constr in constrs["constraints"]:
-    abc = []
-    for i in range(3):
-        t = 0
-        for varn, var in constr[i].items():
-            n = int(varn)
-            v = int(var)
-            if v > p // 2:
-                v = v - p
-            t += vars[n] * v
-        abc.append(t)
-    a, b, c = abc
-    s.add(a * b - c == 0)
-
-for i in range(nout):
-    s.add(vars[i + 1] == out[i])
-
-w = []
-i = 0
-print("__________________________________________________________")
-print("Найденные witness:")
-if argv[1] == "1":
-    if s.check() == sat:
-        m = s.model()
-        for x in vars:
-            w.append(str(m[x]))
-        with open("twitness/witness.json", "wt") as f:
-            json.dump(w, f)
-        with open("data/calculated_input.json", "wt") as f:
-            json.dump([w[x] for x in range(len(w)) if "main.in" in map[x]], f)
+            new = []
+            for x in vars:
+                new.append(x != m[x])
+            s.add(Or(new))
+            w = []
+            i += 1
+            if i > bound:
+                print("too much")
+                exit()
     else:
-        print("unsat")
-else:
-    while s.check() == sat:
-        m = s.model()
-        for x in vars:
-            w.append(str(m[x]))
+        print("unknown option")
 
-        print("".join(w))#len(w))
-        with open(f"calculations/input{str(i).zfill(2)}.json", "wt") as f:
-            json.dump({"in": [w[x] for x in range(len(w)) if "main.in" in map[x]]}, f)
 
-        new = []
-        for x in vars:
-            new.append(x != m[x])
-        s.add(Or(new))
-        w = []
-        i += 1
-        if i > 20:
-            print("too much")
-            exit()
+def verify(bound, flag=True):
+    constrs = json.load(open("constraints/constr.json"))
+    witness = json.load(open("witness/witness.json"))
+    map = json.load(open("data/map.json"))["map"]
+
+    nvars = constrs["nVars"]
+    nout = constrs["nOutputs"]
+    ninp = constrs["nPrvInputs"]
+
+    p = int(constrs["prime"])
+
+    out = [int(x) for x in witness[: 1 + nout]]
+    vars = [BitVec(f"var_{i}", 1) for i in range(nvars)]
+
+    s = Solver()
+    for i in range(nout + 1):  # 1 included
+        s.add(vars[i] == out[i])
+
+    for constr in constrs["constraints"]:
+        abc = []
+        for i in range(3):
+            t = 0
+            for varn, var in constr[i].items():
+                n = int(varn)
+                v = int(var)
+                if v > p // 2:
+                    v = v - p  # TODO track the optimizations
+                t += vars[n] * v
+            abc.append(t)
+        a, b, c = abc
+        s.add(a * b - c == 0)
+
+    if flag:
+        find_all_solutions(s, map, vars, bound)
+    else:
+        return s
+
+
+if __name__ == "__main__":
+    verify(20)
