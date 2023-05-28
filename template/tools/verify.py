3d7b2c3e970a2f89fcb14c63eff406a5e9fd4e91
Sarkoxed <sarkoxed2013@yandex.ru>
2023-05-28 12:38:24 +0300

    unique

--- template/tools/verify.py
+++ template/tools/verify.py
@@ -14,12 +14,12 @@ def find_all_solutions(s, map, vars, bound, ninp, nout):
             for x in vars:
                 w.append(str(m[x]))
 
-            m_inp = "".join([w[i] for i in range(1 + nout, 1 + nout + ninp)])
+            m_inp = [w[i] for i in range(1 + nout, 1 + nout + ninp)]
             print(m_inp)
             with open("twitness/witness.json", "wt") as file:
                 json.dump(w, file)
             with open("data/calculated_input.json", "wt") as file:
-                json.dump([int(x) for x in m_inp], file)
+                json.dump([x for x in m_inp], file)
         else:
             print("unsat")
 
@@ -29,11 +29,11 @@ def find_all_solutions(s, map, vars, bound, ninp, nout):
             for x in vars:
                 w.append(str(m[x]))
 
-            m_inp = "".join([w[i] for i in range(1 + nout, 1 + nout + ninp)])
+            m_inp = [w[i] for i in range(1 + nout, 1 + nout + ninp)]
             print(ninp)
             print(m_inp)
             with open(f"calculations/input{str(i).zfill(2)}.json", "wt") as file:
-                json.dump({"in": [int(x) for x in m_inp]}, file)
+                json.dump({"in": [x for x in m_inp]}, file)
 
             new = []
             for x in vars:
@@ -45,10 +45,10 @@ def find_all_solutions(s, map, vars, bound, ninp, nout):
                 print("too much")
                 exit()
     else:
-        print("unknown option")
+        print("unknown option ", argv[1])
 
 
-def verify(bound=20, flag=True):
+def verify(bound=20, flag=0):
     constrs = json.load(open("constraints/constr.json"))
     witness = json.load(open("witness/witness.json"))
     map = json.load(open("data/map.json"))["map"]
@@ -77,16 +77,38 @@ def verify(bound=20, flag=True):
         a, b, c = abc
         s.add(a * b - c == 0)
 
-    if flag:
+    if flag == 0:
         out = [int(x) for x in witness[: 1 + nOut]]  # out[0] = 1
-        print(out)
         for i in range(nOut + 1):  # 1 included
             s.add(vars[i] == out[i])
-        print(s)
 
         find_all_solutions(s, map, vars, bound, nInp, nOut)
-    else:
+    elif flag == 1:
         return (s, vars)
+    elif flag == 2:
+        vars1 = [BitVec(f"r1cs_var_{i}", 1) for i in range(nvars)]
+        s.add(vars1[0] == 1)
+        for constr in constrs["constraints"]:
+            abc = []
+            for i in range(3):
+                t = 0
+                for varn, var in constr[i].items():
+                    n = int(varn)
+                    v = int(var)
+                    if v > p // 2:
+                        v = v - p  # TODO track the optimizations
+                    t += vars1[n] * v
+                abc.append(t)
+            a, b, c = abc
+            s.add(a * b - c == 0)
+        
+        for i, j in zip(vars, vars1):
+            s.add(i != j)
+        for i in range(nOut + 1):
+            s.add(vars[i] == vars1[i])
+
+        if s.check() == sat:
+            print("Входные данные не уникальны")
 
 
 def check_witness():
